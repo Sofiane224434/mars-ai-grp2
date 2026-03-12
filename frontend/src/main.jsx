5b367b03c090963fa10ddcd7d486a58bd280265f
Azim.404 | Sofiane K <Sb.kherarfa.@gmail.com>
2026-03-12 15:51:09 +0100

    feat: ajouter la prise en charge de l'internationalisation avec i18next et créer des fichiers de traduction pour l'anglais et le français

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './contexts/authcontext.jsx';
 import './index.css';
+import './i18n';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>

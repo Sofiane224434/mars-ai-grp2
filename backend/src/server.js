962a0f61b10401f407c348da1726398ae17e59be
Azim.404 | Sofiane K <Sb.kherarfa.@gmail.com>
2026-03-16 10:59:54 +0100

    refactor: réorganiser la structure du backend en déplaçant les fichiers dans un dossier src

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import { testConnection } from './config/db.js';
 import authRoutes from './routes/auth.routes.js';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Connexion BDD

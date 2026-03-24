5e5590c3e19d876770b4e0b743b88072603d96cf
nina-penot <nina.penot@laplateforme.io>
2026-03-24 14:09:16 +0100

    BUG: checkbox optionnel ne foncitonne pas
    - Le checkbox avec une input optionnelle ne fonctionne pas correctement
    - CORRECTION : Les inputs additifs ne retenaient pas leur value, corrigé

--- frontend/src/components/SimplerFormTest.jsx
+++ frontend/src/components/SimplerFormTest.jsx
@@ -11,14 +11,36 @@ export default function SimplerFormTest() {
 
     function handleChange(e, isgroup = false, groupname = null) {
         const target = e.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        let value;
+        if (target.type === "checkbox") {
+            value = target.checked;
+        } else if (target.type === "file") {
+            console.log(target);
+            value = target.files[0];
+
+            //for getting width and height modify later for video format
+            let reader = new FileReader();
+            reader.onload = function (event) {
+                let image = new Image();
+                image.src = event.target.result;
+
+                image.onload = function () {
+                    console.log(this);
+                }
+            };
+
+            reader.readAsDataURL(value);
+        } else {
+            value = target.value;
+        }
+        //const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
         if (isgroup) {
             if (results[groupname]) {
                 let newres = JSON.parse(JSON.stringify(results));
                 newres[groupname][name] = value;
                 setResults(newres);
-                console.log(results);
+                //console.log(results);
             } else {
                 let obj = { [name]: value };
                 setResults(values => ({ ...values, [groupname]: obj }));
@@ -26,7 +48,15 @@ export default function SimplerFormTest() {
         } else {
             setResults(values => ({ ...values, [name]: value }));
         }
+        console.log(results);
     }
+
+    // function handleConditionalGroup(e, groupname) {
+    //     if(!e.target.checked) {
+    //         let newres = JSON.parse(JSON.stringify(results));
+
+    //     }
+    // }
 
     function addToGroup(groupname) {
         if (!results[groupname]) {
@@ -57,9 +87,28 @@ export default function SimplerFormTest() {
             <form>
                 <div>My first input</div>
                 <input type="text" name="inp1" onChange={handleChange}
-                    value={results["inp1"]}></input>
+                    value={results["inp1"] ? results["inp1"] : ""}></input>
                 <div>My second input</div>
-                <input type="text" name="inp2"></input>
+                <input type="text" name="inp2" onChange={handleChange}
+                    value={results["inp2"] ? results["inp2"] : ""}></input>
+                <div>Upload video/image file</div>
+                <input type="file" name="upload" onChange={handleChange}
+                    accept="image/png, image/jpg, video/mp4, video/mov"
+                    value={results["upload" ? results["upload"] : ""]}
+                    files={results['upload'] ? results["upload"].File : ""}></input>
+                <div>Options test</div>
+                <select name="optionstest" onChange={handleChange}>
+                    <option value={""}></option>
+                    <option value={"first"}>First option</option>
+                    <option value={"second"}>Second option</option>
+                    <option value={"other"}>Other</option>
+                </select>
+                {results["optionstest"] == "other" &&
+                    <div>
+                        <div>Which one?</div>
+                        <input name=""></input>
+                    </div>
+                }
             </form>
         ],
         [
@@ -79,26 +128,40 @@ export default function SimplerFormTest() {
         ],
         [
             <form>
-                <div>Talk about colors?</div>
-                <input type="checkbox" name="checkbox1" onChange={handleChange}></input>
-                {results.checkbox1 &&
+                {/* group name: colors */}
+                <div>Talk about colors? (optional)</div>
+                <input type="checkbox" name="checkbox_color"
+                    onChange={(e) => handleChange(e, true, "colors")}
+                    checked={results["colors"] ?
+                        results["colors"]["checkbox_color"] :
+                        false}></input>
+                {results["color"] && results["color"]["checkbox_color"] &&
                     <div>
                         <div>Enter fave color</div>
-                        <input type="text" name="color"></input>
+                        <input type="text" name="color" onChange={(e) => handleChange(e, true, "colors")}
+                            value={results["colors"]["color"] ?
+                                results["colors"]["color"] : ""}></input>
                     </div>
                 }
                 <div>All your meals; note: additive</div>
                 {/* groupname: meals */}
                 <div>
                     <input type="text" name={0}
-                        onChange={(e) => handleChange(e, true, "meals")}></input>
+                        onChange={(e) => handleChange(e, true, "meals")}
+                        value={results["meals"] ? (results["meals"][0] ?
+                            results["meals"][0] : "") : ""
+                        }></input>
                     {results.meals && Object.keys(results.meals).map((a, i) => {
                         if (i > 0) {
                             return (
                                 <div>
                                     <input type="text" name={i}
-                                        onChange={(e) => handleChange(e, true, "meals")}></input>
-                                    <button type="button" onClick={() => deleteFromResults(i, true, "meals")}>X</button>
+                                        onChange={(e) => handleChange(e, true, "meals")}
+                                        value={results["meals"] ? (results["meals"][i] ?
+                                            results["meals"][i] : "") : ""
+                                        }></input>
+                                    <button type="button" onClick={() => deleteFromResults(i, true, "meals")}>
+                                        (X)</button>
                                 </div>
                             )
                         }

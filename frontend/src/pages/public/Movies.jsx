b13c5b8ee51ab7ad38bdc162b913cb4089603cc4
olivier.parraud <olivier.parraud@laplateforme.io>
2026-03-24 13:21:49 +0100

    feat: Filtre Admin jury (En Travaux)
    Travaux non fini

--- frontend/src/pages/public/Movies.jsx
+++ frontend/src/pages/public/Movies.jsx
@@ -1,14 +1,17 @@
 // pages/Movies.jsx
 import { useTranslation } from "react-i18next";
+import { Filtre } from "../../components/ui/Filtre";
 
 function Movies() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <div className="min-h-screen">
-            <h1>{t("nav.movies")}</h1>
-        </div>
-    );
+  return (
+    <div className="min-h-screen">
+      <h1>{t("nav.movies")}</h1>
+      <Filtre variant="filtre_horizontal"> TEST </Filtre>
+      <Filtre variant="filtre_vertical"> TEST </Filtre>
+    </div>
+  );
 }
 
 export default Movies;

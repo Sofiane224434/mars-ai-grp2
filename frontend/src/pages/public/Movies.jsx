514d0edec51c4719f0bb75a2de326105ce857c29
olivier.parraud <olivier.parraud@laplateforme.io>
2026-03-24 16:43:46 +0100

    fix:bug

--- frontend/src/pages/public/Movies.jsx
+++ frontend/src/pages/public/Movies.jsx
@@ -1,6 +1,5 @@
 // pages/Movies.jsx
 import { useTranslation } from "react-i18next";
-import { PropertyPanel } from "../../components/ui/Panel";
 
 function Movies() {
     const { t } = useTranslation();
@@ -8,7 +7,7 @@ function Movies() {
     return (
         <div className="min-h-screen">
             <h1>{t("nav.movies")}</h1>
-            <PropertyPanel> DASHBORD </PropertyPanel>
+            
         </div>
     );
 }

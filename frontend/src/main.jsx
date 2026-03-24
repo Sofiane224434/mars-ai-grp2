944849fbc7ae046cf2af399cf157d59355f95406
olivier.parraud <olivier.parraud@laplateforme.io>
2026-03-24 10:45:53 +0100

    feat: Ajoute bouton panel jury
    Ajout Valider, Refuser, À revoir
    feat Sofiane

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import './assets/css/customcss.css';
 import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(

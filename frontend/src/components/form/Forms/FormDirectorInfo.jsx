5f533920dedded17d476b35fd51b8acf3e244074
nina-penot <nina.penot@laplateforme.io>
2026-05-06 11:24:58 +0200

    Ajout d'une checkbox newsletter au formulaire
    - Ajout d'une checkbox dans FormDirectorInfo
    - Ajout de logique dans l'envoi du formulaire pour aussi inscrire le réalisateur à la newsletter si il coche la checkbox pour s'inscrire

--- frontend/src/components/form/Forms/FormDirectorInfo.jsx
+++ frontend/src/components/form/Forms/FormDirectorInfo.jsx
@@ -34,6 +34,7 @@ export default function FormDirectorInfo({ hide = false, getFunction,
     const [fixtel, setFixtel] = useState("");
     const [school, setSchool] = useState("");
     const [language, setLanguage] = useState("");
+    const [newsletterCheck, setNewsletterCheck] = useState("");
 
     const [errorFirstname, setErrorFirstname] = useState("");
     const [errorLastname, setErrorLastname] = useState("");
@@ -75,7 +76,8 @@ export default function FormDirectorInfo({ hide = false, getFunction,
         job: job,
         school: school,
         language: language,
-        fixtel: fixtel ? fixtel : null
+        fixtel: fixtel ? fixtel : null,
+        newsletterCheck: newsletterCheck
     }
 
     function sendData() {
@@ -345,6 +347,11 @@ export default function FormDirectorInfo({ hide = false, getFunction,
             <InputSuper type={"checkbox"}
                 getValueFunc={setRulesCheck} required={true}
                 label={t("form.step4.rulesCheck")} errormessage={errorRulesCheck}></InputSuper>
+
+            <InputSuper type={"checkbox"}
+                getValueFunc={setNewsletterCheck} required={true}
+                label={"Je souhaite m'inscrire à la newsletter de MarsAI festival"}
+            ></InputSuper>
 
             <div className="mt-4 flex w-full items-center justify-center gap-4">
                 <Button variant="filled-yellow" interactive type="button" onClick={goback}>{"<"} {t("form.previous")}</Button>
